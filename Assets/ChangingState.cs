4799e779fc362e0f302bf9ebddcb7f89e0c5e7db
sean <sennis3@uic.edu>
2020-10-10 19:59:10 -0500

    Playset almost finished

---
+++ Assets/ChangingState.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangingState : MonoBehaviour
+{
+	Animator anima;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anima = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)) anima.SetTrigger("celebrate");
+        //if (Input.GetKeyDown(KeyCode.F)) anima.SetTrigger("fightsomething");
+    }
+}
+
